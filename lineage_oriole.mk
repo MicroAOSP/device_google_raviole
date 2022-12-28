b004e4e90050573181556cdee7437c3b98a2b313
SevralT <a.chycherin@outlook.com>
2022-12-29 00:58:06 +0100

    raviole: MicroAOSP RAVIOLE Tree Bringup!

--- lineage_oriole.mk
+++ lineage_oriole.mk
@@ -4,10 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Lineage stuff.
-TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 # Inherit device configuration
 $(call inherit-product, device/google/raviole/aosp_oriole.mk)
 $(call inherit-product, device/google/gs101/lineage_common.mk)
@@ -18,10 +14,6 @@ include device/google/raviole/oriole/device-lineage.mk
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 6
 PRODUCT_NAME := lineage_oriole
-
-# Boot animation
-TARGET_SCREEN_HEIGHT := 2400
-TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=oriole \
--- lineage_raven.mk
+++ lineage_raven.mk
@@ -4,10 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Lineage stuff.
-TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 # Inherit device configuration
 $(call inherit-product, device/google/raviole/aosp_raven.mk)
 $(call inherit-product, device/google/gs101/lineage_common.mk)
@@ -18,10 +14,6 @@ include device/google/raviole/raven/device-lineage.mk
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 6 Pro
 PRODUCT_NAME := lineage_raven
-
-# Boot animation
-TARGET_SCREEN_HEIGHT := 3120
-TARGET_SCREEN_WIDTH := 1440
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=raven \
